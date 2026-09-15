aeb782bd117372d0a8b1d5248942d7c32c889ce4
Anant raj <ayushanant16@gmail.com>
2026-09-16 02:22:22 +0530

    Change BACKEND_TUNNEL_URL to new localtunnel link
    Updated the BACKEND_TUNNEL_URL to a new localtunnel link.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 
 const MANAGER_PHONE = "9472948984";
 const NTFY_TOPIC = `retail-vision-${MANAGER_PHONE}`;
-const BACKEND_TUNNEL_URL = "https://yummy-signs-relate.loca.lt"; // Apne localtunnel ka active link
+const BACKEND_TUNNEL_URL = "https://puny-lands-flow.loca.lt";
 
 interface SKUItem {
   id: string;
